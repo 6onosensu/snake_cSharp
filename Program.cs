9164edb75207ace4886e74dfa68a8f2e1995ee3e
Dasha <darja.portnova@gmail.com>
2024-04-30 13:53:36 +0300

    start position coordinate+ draw func

--- Program.cs
+++ Program.cs
@@ -6,7 +6,24 @@ namespace snake_cSharp
     {
         static void Main(string[] args)
         {
+            int x1 = 1;
+            int y1 = 1;
+            char sym1 = '*';
 
+            int x2 = 2; 
+            int y2 = 2;
+            int sym2 = '#';
+
+            Draw(x2, y2, sym2);
+
+
+            Console.ReadLine();
+        }
+
+        static void Draw (int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
